4e37c4f4fb3bcb18b22b8843ea41c33c1016f6e8
Samuel MAUGER <smauger.fr>
2024-04-01 20:22:52 +0100

    Fix python webserver

--- server/server_fp.py
+++ server/server_fp.py
@@ -1,24 +1,33 @@
 #!/usr/bin/env python3
+import binascii
 from http.server import HTTPServer
 from http.server import BaseHTTPRequestHandler
 from http.server import SimpleHTTPRequestHandler
+from io import BytesIO
 import json
 import base64
 import mimetypes
 from PIL import Image
 
 def fingerprint_from_noised(noised: str, r: int, g: int, b: int, a: int):
-    noised_canvas = Image.open(noised)
+    #Delete html tag from noised base64 image (data:image/png;base64)
+    noised = noised.split(',')[1]
+    try:
+        noised_canvas = Image.open(BytesIO(base64.b64decode(noised)))
+    except binascii.Error as e:
+        print("Error decoding base64: ", e)
+        return None
     pix = noised_canvas.load()
     for i in range(noised_canvas.size[0]):
         for j in range(noised_canvas.size[1]):
             pr, pg, pb, pa = pix[i,j]
-
             pix[i,j] = (pr - r, pg - g, pb - b, pa - a)
     
     noised_canvas.save("unnoised.png")
-    data = base64.b64encode(noised_canvas.tobytes()).decode("utf-8")
-    return data
+    buffered = BytesIO()
+    noised_canvas.save(buffered, format="png")
+    img_str = base64.b64encode(buffered.getvalue())
+    return img_str
 
 #Python webserver
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
@@ -38,26 +47,27 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.wfile.write(b"<h1>Error 404: Not Found</h1>")
 
     def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length)
-        post_data = json.loads(post_data)
-        noised = post_data['noised']
-        if(post_data['noise'] == None):
-            print("Without noise: ", noised)
-        else:
-            noise = post_data['noise'].split(',')
-            r = int(noise[0])
-            g = int(noise[1])
-            b = int(noise[2])
-            a = int(noise[3])
-            fingerprint = fingerprint_from_noised(noised, r, g, b, a)
-            print("With noise: ", fingerprint)
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-        self.wfile.write(json.dumps({"fingerprint": fingerprint}).encode())
+        if self.path == '/uploads':
+            content_length = int(self.headers['Content-Length'])
+            post_data = self.rfile.read(content_length)
+            post_data = json.loads(post_data)
+            noised = post_data['image']
+            if "noise" in post_data:
+                noise = post_data['noise'][0].split(',')
+                r = int(noise[0])
+                g = int(noise[1])
+                b = int(noise[2])
+                a = int(noise[3])
+                print("With noise: ", noised)
+                fingerprint = fingerprint_from_noised(noised, r, g, b, a)
+                print("With deleted noise: ", fingerprint)
+            else:
+                print("Without noise: ", noised)
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
 
-httpd = HTTPServer(('localhost', 3000), SimpleHTTPRequestHandler)
+httpd = HTTPServer(('0.0.0.0', 3000), SimpleHTTPRequestHandler)
 #no ssl
 httpd.serve_forever()
 
